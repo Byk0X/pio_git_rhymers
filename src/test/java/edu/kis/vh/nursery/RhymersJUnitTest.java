0b80e2f2c0c9d3562a73114e8877c081f27b29ad
Maciej Lencewicz <maciej.lencewicz@gmail.com>
2024-05-29 12:50:25 +0200

    Dokonanie walidacji testami

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class RhymersJUnitTest {
 
+    //testy przechodzą
     @Test
     public void testCountIn() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
